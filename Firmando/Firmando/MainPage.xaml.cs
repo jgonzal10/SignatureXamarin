e866a326eecb96ff54453b3b41e2f985ee793925
Jennifer Gonzales Alzate <jennifer.alzate@RSB-MACJENNIFER.local>
2018-06-06 09:37:07 -0300

    xamarinFormsSignature

---
+++ Firmando/Firmando/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignaturePad.Forms;
+using Xamarin.Forms;
+
+namespace Firmando
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveButton_Clicked(object sender, System.EventArgs e)
+        {
+            Stream image = await PadView.GetImageStreamAsync(SignatureImageFormat.Jpeg); 
+        }
+
+        private void ClearButton_Clicked(object sender, System.EventArgs e)
+        {
+            PadView.Clear();
+        }
+    }
+}
