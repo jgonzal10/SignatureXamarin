cd6f9e2f2d000e2eb54db0d5bf49a68a68745bd2
Jennifer Gonzales Alzate <jennifer.alzate@RSB-MACJENNIFER.local>
2018-06-07 16:40:51 -0300

    webviewMygithub

---
+++ WebViewCross/WebViewCross/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebViewCross
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Browser.HeightRequest = 1000;
+            Browser.WidthRequest = 1000;
+            Browser.Source = "http://github.com/jgonzal10";
+        }
+
+        private void webOnNavigating(Object sender, WebNavigationEvent e){
+            lblStatus.Text = "Loading Git";
+
+        }
+        private void webOnEndNavigating(Object sender, WebNavigationEvent e){
+            lblStatus.Text = "Done!!";
+        }
+    }
+}
